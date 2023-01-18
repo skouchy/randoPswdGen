fd7afcb33268702639e5d638cc46010a7baa1e79
skouch <skouchy24@gmail.com>
2023-01-18 02:26:57 -0700

    feat(final): Final product with all functions doing all of the functioning things their functioned to function.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # randoPswdGen
-<!-- An online Portfolio used to host and highlight my expanding web dev skills through deliberate applications completed as a student, while providing a positive experience to a visiting user through finer details with a touch of cheeky personality.  -->
+Random Password Generator
 
 ## Description
-<!-- An online portfolio created as a hub to store, display, and build on as I continue to develop my Web Development experiences through applications. I have built this page utilizing a variety of CSS properties including: a hero-background image, positional attributes, navigation links, hover effects, both flex and grid displays, color gradients, and so much more. "Good things take time".  -->
+A simply designed webpage with the ability to generate a randomized password at the "click" of a button. Users will first be prompted to choose the length of their Password with a required minimum of 8 characters and max of 128. Once length is determined, the user will be able to confirm or decline a series of options to include lowercase, uppercase, numerical, and/or special character-types - but they must choose at least 1! Immediately after input results are submitted, a randomized set of characters, of a [valid] desired length, are displayed in the once blank text area, just above the red "Generate Password" button.   
 
 ## Built With
 * HTML
+* CSS
 * javaScript
 
 ## Link
-<!-- https://skouchy.github.io/proPortfolio/ -->
+https://github.com/skouchy/randoPswdGen
 
 ### Screenshot
-<!-- ![portfolioScreenShot](https://user-images.githubusercontent.com/119292219/211481253-4ad04cf0-408b-411f-ab07-1853d2532557.png) -->
+![10 0 0 99_5500_index html (1)](https://user-images.githubusercontent.com/119292219/213125553-61aabc29-2252-46c3-96c3-60c776073ff1.png)
--- script.js
+++ script.js
@@ -8,92 +8,74 @@ const upperCase = [...alpha.toString().toUpperCase()];
 const numChar = [...numeric.toString()];
 const specialChar = [...special.toString()];
 
-
-// Assignment Code
+// * Provided assignment Code * //
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// * Event listener to #generate.btn on mouse "Click" * //
+generateBtn.addEventListener("click", writePassword);
+
+
+// * Receives {generatePassword} data to input generated characters into #password text-area * //
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    
     passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
+// * Iterates data based on user input only after validating all conditions * //
 function generatePassword() {
+    let genPassword = '';
     let charArray = [];
     let choiceLength = prompt("How many characters would you like your password to include? (Must be a number 8 - 128)", '');
-    
+
+    //* Validates character limit condition in order to continue *//
     if (choiceLength >= 8 && choiceLength <= 128) {
-        console.log(choiceLength);
-        
+        console.log(choiceLength); // "if" wraps all conditionals, "else" is below to recall function if null//
+
+    //* Character-types: True values (user confirms) join final array group *//    
         let choiceLower = confirm("Would you like your password to include lowercase characters?");
         if (choiceLower) {
             charArray = lowerCase.concat(charArray);
             console.log(charArray);
         }
-        
+
         let choiceUpper = confirm("Would you like your password to include UPPERCASE characters?");
         if (choiceUpper) {
             charArray = upperCase.concat(charArray);
             console.log(charArray);
         }
-        
+
         let choiceNumeric = confirm("Would you like your password to include NUM8ER5?");
         if (choiceNumeric) {
             charArray = numChar.concat(charArray);
             console.log(charArray);
         }
-        
+
         let choiceSpecial = confirm("Would you like your password to include $pec!@l characters?");
         if (choiceSpecial) {
             charArray = specialChar.concat(charArray);
             console.log(charArray);
         }
-        
+
+        //* Validates requirement that a minimum of one character-type is chosen *//
         if (!choiceLower && !choiceUpper && !choiceNumeric && !choiceSpecial) {
             alert("Bummer. You must choose at least one character-type! Start over.");
             generatePassword();
         }
     }
-    
+
+    //* Recalls {generatePassword} if character limit(user input) is invalid *//
     else {
         alert("You must choose a valid number between 8 and 128!")
         generatePassword();
     }
 
-    // for (let index = 0; index < choiceLength; index++) {
-    //     var genPassword = charArray[Math.floor(Math.random() * charArray.length)];
-    //     console.log(genPassword);
-    // }
+    //* Generates randomized password : user's choice of length and character-types *//
+    for (let index = 0; index < choiceLength; index++) {
+        var charLoops = charArray[Math.floor(Math.random() * charArray.length)];
+        console.log(charLoops);
+        genPassword = genPassword + charLoops;
+    }
+    return genPassword;
 }
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// console.log("=========lowerCase=========");
-// console.log(lowerCase);
-// console.log("===========upperCase===========");
-// console.log(upperCase);
-// console.log("===============numString===========");
-// console.log(numChar);
-// console.log("===============$peci@l===========");
-// console.log(specialChar);
-// * userInput = parseInt(choiceLength);
