d79bdc10a25e8b52e67a26eb2a44119e87e1741a
skouch <skouchy24@gmail.com>
2023-01-17 23:13:26 -0700

    feat(conditionals): All functions and conditionals are functioning. Fail safes and alerts are in place and looping back to local parent function as needed. User input for length is self-mitigating integers within required conditions. The array.concat to open placeholder array for all true booleans and their individual arrays of strings are concating as planned. Remaining needs is putting for-loop for random generation, and final touches to comments, readme, and a cleanup of test folder and codes.

--- script.js
+++ script.js
@@ -4,20 +4,9 @@ const numeric = ["1234567890"];
 const special = ["!/#$%&'()*+,-.:;<=>?@\"[]^_`{|}~"];
 
 const lowerCase = [...alpha.toString()];
-// console.log("=========lowerCase=========");
-// console.log(lowerCase);
-
 const upperCase = [...alpha.toString().toUpperCase()];
-// console.log("===========upperCase===========");
-// console.log(upperCase);
-
 const numChar = [...numeric.toString()];
-// console.log("===============numString===========");
-// console.log(numChar);
-
 const specialChar = [...special.toString()];
-// console.log("===============$peci@l===========");
-// console.log(specialChar);
 
 
 // Assignment Code
@@ -25,14 +14,63 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    console.log("we made it to the fkn writepass fukntion");
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+    
     passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    let charArray = [];
+    let choiceLength = prompt("How many characters would you like your password to include? (Must be a number 8 - 128)", '');
+    
+    if (choiceLength >= 8 && choiceLength <= 128) {
+        console.log(choiceLength);
+        
+        let choiceLower = confirm("Would you like your password to include lowercase characters?");
+        if (choiceLower) {
+            charArray = lowerCase.concat(charArray);
+            console.log(charArray);
+        }
+        
+        let choiceUpper = confirm("Would you like your password to include UPPERCASE characters?");
+        if (choiceUpper) {
+            charArray = upperCase.concat(charArray);
+            console.log(charArray);
+        }
+        
+        let choiceNumeric = confirm("Would you like your password to include NUM8ER5?");
+        if (choiceNumeric) {
+            charArray = numChar.concat(charArray);
+            console.log(charArray);
+        }
+        
+        let choiceSpecial = confirm("Would you like your password to include $pec!@l characters?");
+        if (choiceSpecial) {
+            charArray = specialChar.concat(charArray);
+            console.log(charArray);
+        }
+        
+        if (!choiceLower && !choiceUpper && !choiceNumeric && !choiceSpecial) {
+            alert("Bummer. You must choose at least one character-type! Start over.");
+            generatePassword();
+        }
+    }
+    
+    else {
+        alert("You must choose a valid number between 8 and 128!")
+        generatePassword();
+    }
+
+    // for (let index = 0; index < choiceLength; index++) {
+    //     var genPassword = charArray[Math.floor(Math.random() * charArray.length)];
+    //     console.log(genPassword);
+    // }
+}
+
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
@@ -41,44 +79,21 @@ generateBtn.addEventListener("click", writePassword);
 // THEN I select which criteria to include in the password
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
-function generatePassword() {
-    let randomChar = '';
-    let choiceLength = prompt("How long would you like your password to be?(Must be a number 8-128)", '');
-
-    if (choiceLength >= 8 && choiceLength <= 128) {
-        console.log(choiceLength);
-        // userInput = parseInt(choiceLength);
-
-
-        // TODO: WHEN asked for character types to include in the password
-        // TODO: THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-        let choiceLower = confirm("Would you like your password to include lowercase characters?");
-        let choiceUpper = confirm("Would you like your password to include UPPERCASE characters?");
-        let choiceNumeric = confirm("Would you like your password to include numbers?");
-        let choiceSpecial = confirm("Would you like your password to include $pec!@l (special) characters?");
-
-
-        if (choiceLower) {
-            console.log(true);
-        }
-        if (choiceUpper) {
-            console.log("ALPHA-DADDY");
-        }
-        if (choiceNumeric) {
-            console.log("lemme get dat numeric");
-        }
-        if (choiceSpecial) {
-            console.log(specialChar);
-        }
-    }
-    else {
-        alert("You must choose a valid number between 8-128!");
-        generatePassword ();
-    }
-}
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+// console.log("=========lowerCase=========");
+// console.log(lowerCase);
+// console.log("===========upperCase===========");
+// console.log(upperCase);
+// console.log("===============numString===========");
+// console.log(numChar);
+// console.log("===============$peci@l===========");
+// console.log(specialChar);
+// * userInput = parseInt(choiceLength);
--- testConsole/script.js
+++ testConsole/script.js
@@ -1,15 +1,15 @@
-let theFinalCharSet = '';
+// let theFinalCharSet = '';
 
 
-const alpha = ["abcdefghijklmnopqrstuvwxyz"];
-const numeric = ["1234567890"];
-const special = ["!/#$%&'()*+,-.:;<=>?@\"[]^_`{|}~"];
+// const alpha = ["abcdefghijklmnopqrstuvwxyz"];
+// const numeric = ["1234567890"];
+// const special = ["!/#$%&'()*+,-.:;<=>?@\"[]^_`{|}~"];
 
 
-const lowerCase = [...alpha.toString()];
-const upperCase = [...alpha.toString().toUpperCase()];
-const numChar = [...numeric.toString()];
-const specialChar = [...special.toString()];
+// const lowerCase = [...alpha.toString()];
+// const upperCase = [...alpha.toString().toUpperCase()];
+// const numChar = [...numeric.toString()];
+// const specialChar = [...special.toString()];
 
 // * const choiceLower = true;
 // * const choiceLength = 9; 
@@ -17,21 +17,21 @@ const specialChar = [...special.toString()];
 // alpha, numeric, and special are the prototypes?
 
 // * const choiceLower = confirm("Would you like your password to include lowercase characters?");
-if (isChoiceLower) {
-    console.log("little alpha");
-}
+// if (isChoiceLower) {
+//     console.log("little alpha");
+// }
 
-// if (choiceLower) {
-//  for (let index = 0; index < choiceLength; index++) {
-//      console.log(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
-//     // const element = array[index];
+if (charArray) {
+ for (let index = 0; index < choiceLength; index++) {
+     console.log(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
+    // const element = array[index];
     
-//  }
-// } // T or F
+ }
+} // T or F
 
 // if (choiceUpper) {
 //     for (let index = 0; index < upperCase.length; index++) {
-    //         const element = array[index];
+//             const element = array[index];
         
 //     }
 // }
